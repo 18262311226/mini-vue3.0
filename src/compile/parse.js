5fdc58e6fbfa8c99f971a0cb11da199380ee6f6c
无为 <3435516833@qq.com>
2022-08-07 01:13:44 +0800

    ast语法树生成

--- src/compile/parse.js
+++ src/compile/parse.js
@@ -14,6 +14,9 @@ function createParseContext(template){
 
 function isEnd (context) {
     const source = context.source
+    if (context.source.startsWith("</")) {
+        return true
+    }
     return !source
 }
 
@@ -46,6 +49,13 @@ function advanceBy (context, endIndex) {
     advancePositionWithMutation(context, source, endIndex)
 
     context.source = source.slice(endIndex)
+}
+
+function advanceBySpaces (context) {
+    let match = /^[ \t\r\n]+/.exec(context.source)
+    if(match){
+        advanceBy(context, match[0].length)
+    }
 }
 
 function parseTextData (context, endIndex) {
@@ -93,11 +103,127 @@ function parseInterpolation (context) {
     const closeIndex = context.source.indexOf("}}", "{{".length)
 
     advanceBy(context, 2)
+
+    const innerStart = getCursor(context)
+    const innerEnd = getCursor(context)
+
+    const rawContextLength = closeIndex - 2
+    let preContent = parseTextData(context, rawContextLength)
+    let content = preContent.trim()
+    let startOffset = preContent.indexOf(content)
+
+    if(startOffset > 0) {
+        advancePositionWithMutation(innerStart, preContent, startOffset)
+    }
+
+    let endOffset = startOffset - content.length
+    advancePositionWithMutation(innerEnd, preContent, endOffset)
+    advanceBy(context, 2)
+
+    return {
+        type: NodeType.INTERPOLATION,
+        content: {
+            type: NodeType.SIMPLE_EXPRESSION,
+            content,
+            loc: getSelection(context, innerStart, innerEnd)
+        },
+        loc: getSelection(context, start)
+    }
 }
 
-function parse (template) {
-    const context = createParseContext(template)
+function parseAttributeValue (context) {
+    const start = getCursor(context)
 
+    let quote = context.source[0]
+    let content
+    if (quote == '"' || quote == "'") {
+        advanceBy(context, 1)
+        const endIndex = context.source.indexOf(quote)
+        content = parseTextData(context,endIndex)
+        advanceBy(context, 1)
+    }
+
+    return {
+        content,
+        loc: getSelection(context, start)
+    }
+}
+
+function parseAttribute (context) {
+    const start = getCursor(context)
+    let match = /^[^\t\r\n\f />][^\t\r\n\f />=]*/.exec(context.source)
+    let name = match[0]
+
+    advanceBy(context, name.length)
+    advanceBySpaces(context)
+    advanceBy(context, 1)
+
+    let value = parseAttributeValue(context)
+
+    return {
+        type: NodeType.ATTRIBUTE,
+        name,
+        value: {
+            type: NodeType.TEXT,
+            ...value
+        },
+        loc: getSelection(context, start)
+    }
+}
+
+function parseAttributes (context) {
+    const props = []
+
+    while (context.source.length > 0 && !context.source.startsWith(">")) {
+        const prop = parseAttribute(context)
+        props.push(prop)
+        advanceBySpaces(context)
+    }
+
+
+    return props
+}
+
+function parseTag (context) {
+    const start = getCursor(context)
+
+    const match = /^<\/?([a-z][^ \t\r\n/>]*)/.exec(context.source)
+    const tag = match[1]
+
+    advanceBy(context, match[0].length)
+    advanceBySpaces(context)
+
+    let props = parseAttributes(context)
+
+    let isSelfClosing = context.source.startsWith("/>")
+
+    advanceBy(context, isSelfClosing ? 2 : 1)
+
+    return {
+        type: NodeType.ELEMENT,
+        tag,
+        isSelfClosing,
+        children: [],
+        loc: getSelection(context, start)
+    }
+}
+
+function parseElement (context) {
+    let ele = parseTag(context)
+
+    let children = parseChildren(context)
+
+    if (context.source.startsWith("</")) {
+        parseTag(context)
+    }
+
+    ele.loc = getSelection(context, ele.loc.start)
+    ele.children = children
+
+    return ele
+}
+
+function parseChildren (context) {
     let nodes = []
     while (!isEnd(context)) {
         let node
@@ -105,11 +231,10 @@ function parse (template) {
         if (source.startsWith("{{")) {
             node = parseInterpolation(context)
         }else if (source[0] === "<") {
-
+            node = parseElement(context)
         }
 
         if (!node) {
-            console.log(2321)
             node = parseText(context)
         }
 
@@ -118,4 +243,10 @@ function parse (template) {
     }
 
     return nodes
+}
+
+function parse (template) {
+    const context = createParseContext(template)
+
+    return parseChildren(context)
 }
